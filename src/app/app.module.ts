2baa44f2e89e8b52148aff184bd20a1cb04ee3cc
ritikakri0312 <ritikakri0312@gmail.com>
2023-07-20 17:41:40 +0530

    ghjkdsacvbnm,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import {RatingModule} from 'ng-starrating';
+import { SearchComponent } from './search/search.component';
+import { FormModule } from './form/form.module';
+
+//  import {RatingModule} from 'ng-starrating';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    SearchComponent,
+   
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RatingModule
+    FormModule
+    // RatingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component,OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/model/food';
-import { StarRatingComponent } from 'ng-starrating';
+// import { StarRatingComponent } from 'ng-starrating';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,9 +10,16 @@ import { StarRatingComponent } from 'ng-starrating';
 })
 export class HomeComponent implements OnInit {
   foods:Foods[] = [];
-  constructor(private fs:FoodService){ }
+  constructor(private fs:FoodService , private router:ActivatedRoute){ }
   ngOnInit(): void {
+    this.router.params.subscribe(params =>{
+      if(params['searchItem'])
+      this.foods = this.fs.getAll().filter(food => food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
+
+      else 
       this.foods = this.fs.getAll();
+    })
+     
   }
 
 }
