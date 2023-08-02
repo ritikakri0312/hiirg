b2a175596f7e0a3c4ba7c97f83f9cb6a27bd1fbe
ritikakri0312 <ritikakri0312@gmail.com>
2023-08-02 19:35:30 +0530

    fbnjf

--- src/app/components/pages/cart-page/cart-page.component.ts
+++ src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,24 +1,36 @@
 import { Component,OnInit } from '@angular/core';
 import { CartService } from 'src/app/cart.service';
+import { CartItem } from 'src/app/shared/CartItem';
 import { Cart } from 'src/app/shared/model/Cart';
+
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.css']
 })
+
 export class CartPageComponent implements OnInit {
   cart!: Cart;
    
   constructor(private cartService: CartService) {
-    this.cartService.getCartObservable().subscribe((cart: Cart)=>{
+    this.cartService.getCartObservable().subscribe((cart: Cart) => {
       this.cart = cart;
     })
   }
 
   ngOnInit():void{
 
-  }
+   }
+
+   removeFromCart(cartItem:CartItem){
+    this.cartService.removeFromCart(cartItem.food.id);
+   }
+
+   changeQuantity(cartItem:CartItem,quantityInString:string){
+    const quantity  = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id,quantity);
+   }
   
 
 }
