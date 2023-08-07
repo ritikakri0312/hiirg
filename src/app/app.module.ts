56e90423a6bbdf3b8ae4423a8aa4662d9d45d465
ritikakri0312 <ritikakri0312@gmail.com>
2023-08-08 03:27:21 +0530

    dcvbnkujm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,9 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { ToastrModule } from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { InputContainerComponent } from './components/partials/input-container/input-container.component'
+import { InputContainerComponent } from './components/partials/input-container/input-container.component';
+import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
+import { TextInputComponent } from './components/partials/text-input/text-input.component'
 
 
 //  import {RatingModule} from 'ngx-bootstrap/rating';
@@ -41,6 +43,8 @@ import { InputContainerComponent } from './components/partials/input-container/i
     NotFoundComponent,
     LoginPageComponent,
     InputContainerComponent,
+    InputValidationComponent,
+    TextInputComponent,
    
     
   ],
