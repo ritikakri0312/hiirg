d2785d9aaffc9e16d522dbcfa6907f68e790394c
ritikakri0312 <ritikakri0312@gmail.com>
2023-08-08 02:44:03 +0530

    hlofamily

--- src/app/components/partials/input-container/input-container.component.ts
+++ src/app/components/partials/input-container/input-container.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input , OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-input-container',
+  selector: 'input-container',
   templateUrl: './input-container.component.html',
   styleUrls: ['./input-container.component.css']
 })
-export class InputContainerComponent {
+export class InputContainerComponent implements OnInit {
+  
+@Input()
+label!:string;
+
+@Input()
+bgColor = 'white';
+
+constructor(){}
+
+ngOnInit():void{
+
+}
 
 }
