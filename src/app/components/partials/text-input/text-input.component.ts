b168217be172b8953829400e1e16375277da05c6
ritikakri0312 <ritikakri0312@gmail.com>
2023-08-08 04:13:56 +0530

    bnm,

--- src/app/components/partials/text-input/text-input.component.ts
+++ src/app/components/partials/text-input/text-input.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
+import { AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-text-input',
+  selector: 'text-input',
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.css']
 })
-export class TextInputComponent {
+export class TextInputComponent implements OnInit {
+  
+  @Input()
+  control!:AbstractControl;
+
+  @Input()
+  showErrorsWhen:boolean = true;
+
+  @Input()
+  label!:string;
+
+  @Input()
+  type: 'text' | 'password' | 'email' = 'text';
+  
+ get formControl(){
+  return this.control as FormControl;
+ }
+
+  constructor(){}
+  
+  ngOnInit(): void {
+    
+  }
 
 }
